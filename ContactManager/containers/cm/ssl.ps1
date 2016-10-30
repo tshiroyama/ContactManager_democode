5df4ee1a88fc1dd43a8f5af1fd3806ef55bd0fff
tshiroyama <tak-shiroyama@outlook.com>
2016-10-31 00:52:04 +0900

    fix scripts

--- ContactManager/containers/cm/ssl.ps1
+++ ContactManager/containers/cm/ssl.ps1
@@ -1,4 +1,4 @@
-import-module WebAdministration
+gimport-module WebAdministration
 
-Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=testsite.local"} | new-item IIS:\SslBindings\0.0.0.0!443
+Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=appite.local"} | new-item IIS:\SslBindings\0.0.0.0!443
 new-webbinding -Name "Default Web Site" -Protocol https -Port 443 -IPAddress "*"
