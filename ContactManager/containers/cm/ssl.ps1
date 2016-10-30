747b4c12472f8570d4c3f3f542e0c29b9fe0a749
tshiroyama <tak-shiroyama@outlook.com>
2016-10-31 00:54:21 +0900

    spell miss

--- ContactManager/containers/cm/ssl.ps1
+++ ContactManager/containers/cm/ssl.ps1
@@ -1,4 +1,4 @@
-gimport-module WebAdministration
+import-module WebAdministration
 
 Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -eq "CN=appite.local"} | new-item IIS:\SslBindings\0.0.0.0!443
 new-webbinding -Name "Default Web Site" -Protocol https -Port 443 -IPAddress "*"
